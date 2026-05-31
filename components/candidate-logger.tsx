fb9e4d888b729cc4b825faa0c5fa3170308225db
100hard <dubeysauhard999@gmail.com>
2026-05-31 13:33:39 +0530

    feat: configure real Candidate LinkedIn profile URL verification in CandidateLogger

--- components/candidate-logger.tsx
+++ components/candidate-logger.tsx
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 export function CandidateLogger() {
   useEffect(() => {
     // Mandatory candidate verification console logging rule
-    console.log("[NextFlow] Candidate LinkedIn: https://www.linkedin.com/in/sauhard-iaoc");
+    console.log("[NextFlow] Candidate LinkedIn: https://www.linkedin.com/in/sauhard-dubey-631a8b1b6/");
   }, []);
 
   return null;
